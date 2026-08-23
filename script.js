5265ccc6781f385079856f2f81e265c9bf933e9c
weryl74-eng <weryl74@gmail.com>
2026-08-23 15:48:02 +0100

    Remove duplicate navigation stylesheet override

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ finalStyles.rel = 'stylesheet';
 finalStyles.href = 'homepage-22aug-final.css';
 document.head.appendChild(finalStyles);
 
-const navStyles = document.createElement('link');
-navStyles.rel = 'stylesheet';
-navStyles.href = 'navigation-reference.css';
-document.head.appendChild(navStyles);
-
 const searchButton = document.querySelector('.search-toggle');
 
 searchButton?.addEventListener('click', () => {
